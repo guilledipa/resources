3f8362482b9d1f5659dc107f0c6bb9748991d2e1
guilledipa <guillermodp@google.com>
2022-09-04 21:03:42 +1000

    Fix package

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
-//go:generate go run github.com/hajimehoshi/file2byteslice/cmd/file2byteslice@v1.0.0 -package=resources -input ./images/spritesheet.png -output ./images/spritesheet.go -var Spritesheet_png
+//go:generate go run github.com/hajimehoshi/file2byteslice/cmd/file2byteslice@v1.0.0 -package=images -input ./images/spritesheet.png -output ./images/spritesheet.go -var Spritesheet_png
 
 package resources
